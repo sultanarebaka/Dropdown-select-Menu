966275bcfb0414ad8727cc079e62d60c1fde2bd5
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-12 14:28:45 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ sBtn_text = optionMenu.querySelector(".sBtn-text");
 
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 options.forEach(option =>{
-    option.addEventListener("click", ()=>
+    option.addEventListener("click", ()=>{
+    let selectedOption = option.querySelector(".option-text").innerText;
 
 
 
