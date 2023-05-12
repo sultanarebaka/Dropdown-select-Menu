a98f330e244db17b8229096fce808d6a0387f265
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-12 06:51:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const optionMenu = document.querySelector(".select-menu"),
+selectBtn = optionMenu.querySelector(".select-btn"),
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+
+
+
+
+
+
+
+
+
